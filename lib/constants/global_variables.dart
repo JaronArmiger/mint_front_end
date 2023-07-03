581ac6dfa31f41bbd789516bcbb45f77ae336f79
Jaron Armiger <jaron.armiger@gmail.com>
2023-07-03 16:34:18 -0700

    create SnackbarGlobal

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,1 +1,13 @@
-class GlobalVariables {}
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  static const appBarGradient = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 9, 135, 102),
+      Color.fromARGB(255, 84, 235, 167),
+    ],
+    stops: [0.2, 1.0],
+  );
+
+  static const darkGreen = Color.fromRGBO(9, 135, 102, 1);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mint_front_end/common/snackbar_global.dart';
+import 'package:mint_front_end/features/home/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scaffoldMessengerKey: SnackbarGlobal.key,
+      debugShowCheckedModeBanner: false,
       title: 'Mint',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
